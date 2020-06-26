301399c9b3b910c8620489c72e0b263169fdc5b0
saiteja <saiteja@saiteja7.mshome.net>
2020-06-26 08:29:27 +0530

    commit 1

---
+++ code2/src/code2/ascii.java
@@ -0,0 +1,14 @@
+package code2;
+
+public class ascii {
+	public static void main(String args[])
+	{
+	 int num[] = {65, 120, 98, 75, 115};
+	        String str =null;
+	        for(int i: num){
+	            str = Character.toString((char)i);
+	            System.out.print(str+" ");
+	        }
+	}
+
+}
