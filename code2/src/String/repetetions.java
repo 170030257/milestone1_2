3d262a6b9d07b730219b67175fbfcf3a8ede559e
saiteja <saiteja@saiteja7.mshome.net>
2020-07-01 20:47:38 +0530

    10

---
+++ code2/src/String/repetetions.java
@@ -0,0 +1,18 @@
+package String;
+
+public class repetetions {
+	public static void main(String[] args) {
+        String a,c="";
+        int b,len;
+        b=Integer.parseInt(args[1]);
+        len=args[0].length();
+        a=args[0];
+        len=len-b;
+        for(int i= 0;i<b;i++)
+        {
+            c=c+a.substring(len);
+        }
+        System.out.println(c);
+
+    }
+}
