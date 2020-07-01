15adba25e33c102ee0241436c4db597debe23fe7
saiteja <saiteja@saiteja7.mshome.net>
2020-07-01 18:40:55 +0530

    6

--- code2/src/inheritance/testemployee.java
+++ code2/src/inheritance/testemployee.java
@@ -1,5 +1,15 @@
 package inheritance;
 
 public class testemployee {
+	public static void main(String args[])
+	{
+	Employee e=new Employee();
+	e.setsalary(10225);
+	e.setys(2000);
+	e.setNIN("ys2345");
+	e.setname("krishna");
+	 System.out.println(e.getname()+" "+e.getNIN()+" "+e.getsalary()+" "+e.getys());
+	
 
+}
 }
